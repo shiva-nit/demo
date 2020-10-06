a537b499be45839b948a8e61e94afec5aa1e0734
shiva-nit <anmol>
2020-10-06 14:48:43 +0530

    Employee.java updated

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -6,30 +6,5 @@ public class Employee {
 	private Double empSal;
 	private Double empAddr;
 
-	public Employee() {
-		super();
-	}
-	public Integer getEmpId() {
-		return empId;
-	}
-	public void setEmpId(Integer empId) {
-		this.empId = empId;
-	}
-	public String getEmpName() {
-		return empName;
-	}
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-	public Double getEmpSal() {
-		return empSal;
-	}
-	public void setEmpSal(Double empSal) {
-		this.empSal = empSal;
-	}
-	@Override
-	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + "]";
-	}
 
 }
